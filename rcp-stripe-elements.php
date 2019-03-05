a69847c9f2374ba518907d56534681bb687f2636
Chris Van Patten <hello@chrisvanpatten.com>
2019-03-05 10:58:47 -0500

    Version bump

--- rcp-stripe-elements.php
+++ rcp-stripe-elements.php
@@ -3,7 +3,7 @@
 Plugin Name: Restrict Content Pro Stripe Elements Gateway
 Plugin URI: http://www.vanpattenmedia.com/
 Description: Accept payments using Stripe Elements
-Version: 1.2.3
+Version: 1.3.0
 Text Domain: rcp-stripe-elements
 Domain Path: /languages
 Author: Van Patten Media Inc.
